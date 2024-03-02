7daa251de0dc79aa989ededcf552a89cc7c547ed
vladudin1 <160415615+vladudin1@users.noreply.github.com>
2024-03-02 13:14:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+        int n = 0;
+    int del = 0;
+    if (a < b) {
+        del = a;
+        if (b % a == 0) {
+            del = a;
+        }
+        else {
+            for (int i = del; i > 0; i--) {
+                if (a % i == 0 && b % i == 0) {
+                    del = i;
+                    break;
+                }
+            }
+        }
+    }
+    if (a > b) {
+        del = b;
+        if (a % b == 0) {
+            del = b;
+        }
+        else {
+            for (int i = del; i > 0; i--) {
+                if (a % i == 0 && b % i == 0) {
+                    del = i;
+                    break;
+                }
+            }
+        }
+    }
+    return del;
 }
